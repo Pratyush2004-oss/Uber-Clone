3d6f46ada1a45a5e92deee58447145da60719a8e
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-02-03 22:29:00 +0530

    Adding functionalities

---
+++ frontend/src/components/captain/RidePopUp.jsx
@@ -0,0 +1,51 @@
+import { ChevronDown, CreditCard, IndianRupee, LocateIcon, MapPin } from 'lucide-react'
+import React from 'react'
+
+const RidePopUp = (props) => {
+    return (
+        <div className='flex flex-col justify-between gap-1 p-5 shadow-lg mx-3 bg-gray-50 border-2 active:border-black rounded-lg my-2'>
+            <div className='text-2xl text-center flex items-center justify-center'><ChevronDown className='size-6' onClick={() => { props.setridePopUp(false) }} /></div>
+            <h3 className='text-xl font-semibold  mb-5'>New Ride Available!</h3>
+            <div className='flex items-center justify-between mb-2 bg-yellow-400 p-3 rounded-lg'>
+                <div className='flex items-center gap-3'>
+                    <div className="avatar ">
+                        <div className="w-10 rounded-full shadow-lg">
+                            <img src='userIcon.jpg' />
+                        </div>
+                    </div>
+                    <h2 className='text-2xl font-semibold'>Harsh Patel</h2>
+                </div>
+                <p className='font-medium text-lg'>2.2 km</p>
+            </div>
+            <div className='w-full px-3 gap-2 flex flex-col justify-between'>
+                {/* Pickup point */}
+                <div className='flex items-center gap-4 border-b-2 py-2 border-t-2 '>
+                    <MapPin className='size-6' />
+                    <div className='flex flex-col gap-1'>
+                        <h1 className='text-xl font-bold flex items-center'>562/11-A</h1>
+                        <p className='text-sm text-gray-600 font-medium'>Karnataka</p>
+                    </div>
+                </div>
+                {/* Destination point */}
+                <div className='flex items-center gap-4 border-b-2 py-2 '>
+                    <LocateIcon className='size-6' />
+                    <div className='flex flex-col gap-1'>
+                        <h1 className='text-xl font-bold flex items-center'>Third Eave Coffee</h1>
+                        <p className='text-sm text-gray-600 font-medium'>Karnataka</p>
+                    </div>
+                </div>
+                {/* Price */}
+                <div className='flex items-center gap-4 border-b-2 py-2'>
+                    <CreditCard className='size-6' />
+                    <div className='flex flex-col gap-1'>
+                        <h1 className='text-xl font-bold flex items-center'><IndianRupee className='size-5 mt-1' />{props.price}</h1>
+                        <p className='text-sm text-gray-600 font-medium'>Cash / UPI</p>
+                    </div>
+                </div>
+            </div>
+            <button className='btn w-full mt-5 bg-green-500 text-white'>Confirm</button>
+            <button className='btn w-full mt-2 bg-gray-300 text-black' onClick={() => props.setridePopUp(false)}>Ignore</button>
+        </div>)
+}
+
+export default RidePopUp
--- frontend/src/pages/CaptainDashboard.jsx
+++ frontend/src/pages/CaptainDashboard.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import CaptainDetails from '../components/captain/CaptainDetails'
+import RidePopUp from '../components/captain/RidePopUp'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const CaptainDashboard = () => {
+    const [ridePopUp, setridePopUp] = useState(true);
+
+    useGSAP(function () {
+        if (ridePopUp) {
+            gsap.to('.RidePopUp', { transform: 'translateY(0)', duration: 1 })
+        }
+        else {
+            gsap.to('.RidePopUp', { transform: 'translateY(100%)', duration: 1 })
+        }
+    }, [ridePopUp])
     return (
         <div className='bg-white/50 p-4 flex flex-col h-screen gap-2'>
             <div className='fixed top-4 right-4 left-4'>
@@ -11,7 +24,11 @@ const CaptainDashboard = () => {
             <div className='h-3/5 rounded-lg'>
                 <img src='/Home.jpg' alt='Home' className='h-full w-full rounded-lg' />
             </div>
-            <CaptainDetails/>
+            <CaptainDetails />
+
+            <div className='fixed w-full z-10 bottom-0 left-0 bg-gray-100 p-2 translate-y-full RidePopUp'>
+                <RidePopUp image={"/car.png"} capacity={4} price={150.25} name={"Uber Go"} setridePopUp={setridePopUp} />
+            </div>
         </div>
     )
 }
